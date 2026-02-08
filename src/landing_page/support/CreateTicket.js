2491696bc55def5f0b747a99ebe602689670e81c
Radheshyam kumar <radheshyamkumark99@gmail.com>
2026-02-08 13:54:12 +0530

    updated all

--- src/landing_page/support/CreateTicket.js
+++ src/landing_page/support/CreateTicket.js
@@ -8,61 +8,61 @@ function CreateTicket() {
       
       <div className="col-4 p-5">
         <h4 className="mb-3"><i class="fa fa-plus-circle"></i> Account Opening</h4>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Online Account Opening</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Offline Account Opening</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Company, Patrnership and HUF Account</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Opening</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>NRI Account Opening</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Charges at Zerodha</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Zerodha IDFC First Bank 3-in-1 Account</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Getting Started</a>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Online Account Opening</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Offline Account Opening</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Company, Patrnership and HUF Account</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Opening</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>NRI Account Opening</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Charges at Zerodha</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Zerodha IDFC First Bank 3-in-1 Account</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Getting Started</button>
       </div>
       <div className="col-4 p-5">
         <h4 className="mb-3"><i class="fa-solid fa-user"></i>Your Zerodha Account</h4>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Login Credentials</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Account Modification and Segment Addition</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>DP ID and Bank details</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Your Profile</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Transfer and Conversion of shares</a> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Login Credentials</button>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Account Modification and Segment </button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>DP ID and Bank details</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Your Profile</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Transfer and Conversion of shares</button> <br/>
       </div>
        <div className="col-4 p-5">
         <h4 className="mb-3"><i class="fa-solid fa-arrow-trend-up"></i> Your Zerodha Account</h4>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Margin/leverage, Product and Order types</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Kite Web and Mobile</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Trading FAQs</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Corporate Actions</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Sentine</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Kite API</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Pi and Other Platforms</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Stocksreports+</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>GTT</a>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Margin/leverage, Product and Order types</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Kite Web and Mobile</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Trading FAQs</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Corporate Actions</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Sentine</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Kite API</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Pi and Other Platforms</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Stocksreports+</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>GTT</button>
       </div>
        <div className="col-4 p-5">
         <h4 className="mb-3"><i class="fa-solid fa-credit-card"></i> Funds</h4>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Adding Funds</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Fund Withdrawal</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>eMandates</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Adding Bank Accounts</a>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Adding Funds</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Fund Withdrawal</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>eMandates</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Adding Bank Accounts</button>
       </div>
        <div className="col-4 p-5">
         <h4 className="mb-3"><i class="fa-regular fa-circle"></i> Console</h4>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Reports</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Ledger</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Portfolio</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>60 Day Challenge</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>IPO</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Referral Program</a> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Reports</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Ledger</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Portfolio</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>60 Day Challenge</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>IPO</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Referral Program</button> <br/>
       </div>
        <div className="col-4 p-5">
         <h4 className="mb-3"><i class="fa-solid fa-piggy-bank"></i> Coin</h4>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Understanding Mutual Funds</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>About Coin</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Buying and Selling Stock</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Starting an SIP</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Managing Your Portfolio</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Coin App</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Moving to Coin</a> <br/>
-        <a href="#" style={{textDecoration:"none",lineHeight:"2.5"}}>Government Securities</a>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Understanding Mutual Funds</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>About Coin</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Buying and Selling Stock</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Starting an SIP</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Managing Your Portfolio</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Coin App</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Moving to Coin</button> <br/>
+        <button className="link-btn" style={{textDecoration:"none",lineHeight:"2.5"}}>Government Securities</button>
       </div>
       </div>
     </div>
