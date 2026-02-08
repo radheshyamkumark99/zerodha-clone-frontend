03756d006650683c472d8d148ee5632657000235
Radheshyam kumar <radheshyamkumark99@gmail.com>
2026-02-08 15:05:35 +0530

    final setup

--- src/index.js
+++ src/index.js
@@ -12,12 +12,14 @@ import Navbar from "./landing_page/Navbar";
 import Footer from "./landing_page/Footer";
 import NotFound from "./landing_page/NotFound";
 import Login from "./landing_page/signup/Login";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Navbar />
     <Routes>
+      <Route element={<App />}></Route>
       <Route path="/" element={<HomePage />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
